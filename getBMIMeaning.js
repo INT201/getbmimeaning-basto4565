d747a39d528f9e45ecac1299ca8ec71ecea91604
PIYAKORN PANICHWONG <piyakorn.basto@mail.kmutt.ac.th>
2022-09-08 14:18:11 +0700

    final commit

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,11 +1,12 @@
 const { template } = require('@babel/core')
+
 function calculateBMI(weight, height) {
   //code here
   return weight / (height*height);
 }
 function getBMIMeaning(weight, height) {
   //code here
-let bmi = calculateBMI
+  let bmi = calculateBMI(weight, height)
  if (bmi < 18.5)  {
   return "Underweight"
 } else if (bmi >= 18.5 && bmi <= 24.9) {
