1f0db313b1d755c9e932206cdf05c1ce62829bfe
PIYAKORN PANICHWONG <piyakorn.basto@mail.kmutt.ac.th>
2022-09-08 14:08:33 +0700

    :sob: final commit

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -5,9 +5,10 @@ function calculateBMI(weight, height) {
 }
 function getBMIMeaning(weight, height) {
   //code here
- if (calculateBMI < 18.5)  {
+  bmi = calculateBMI
+ if (bmi < 18.5)  {
   return "Underweight"
-} else if (calculateBMI >= 18.5 && calculateBMI <=24.9) {
+} else if (bmi >= 18.5 && bmi <=24.9) {
   return "Normal weight"
 } else {
   return "Overweight"
